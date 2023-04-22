a3dca7361ce81bd402c1e4a566c310f3ad7e2a8f
Dillon Richardson <dillonrichardson6@gmail.com>
2023-04-21 23:39:45 -0400

    Added downcase to string_without_spaces in substring function, allowed it to pick up captialized words. Still working on repeated phrases

--- substrings.rb
+++ substrings.rb
@@ -2,25 +2,19 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(word, dictionary)
     hash = {}
-    string_without_spaces = array_to_string(string_to_array(word))
+    string_without_spaces = array_to_string(string_to_array(word)).downcase
     start_index = 0
     end_index = start_index + word.length
 
-    #while end_index < string_without_spaces.length
-        dictionary.each do |entry|
-            string_copy = string_without_spaces
-            hash[entry] = 0
-            string_copy.include?(entry) ? hash[entry] += 1 : hash[entry] += 0
-        end
-        start_index += 1
-        end_index += 1
-    #end
-
+    dictionary.each do |entry|
+        hash[entry] = 0
+        string_without_spaces.include?(entry) ? hash[entry] += 1 : hash[entry] += 0
+    end
     hash.select{|k, v| v > 0}
 end
 
 def string_to_array(string)
-    string.split("")
+    string.split(" ")
 end
 
 def array_to_string(array)
@@ -28,4 +22,4 @@ def array_to_string(array)
     unformatted_string
 end
 
-p substrings("below below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
