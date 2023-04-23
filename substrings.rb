7ae6f6ea4eee7f4c0e7ff00d848853c02edb87b1
Dillon Richardson <dillonrichardson6@gmail.com>
2023-04-23 14:42:17 -0400

    Completed project, it now works with all occurrences of the substring from dictionary

--- substrings.rb
+++ substrings.rb
@@ -1,25 +1,31 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# frozen_string_literal: true
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 def substrings(word, dictionary)
-    hash = {}
-    string_without_spaces = array_to_string(string_to_array(word)).downcase
-    start_index = 0
-    end_index = start_index + word.length
+  hash = {}
+  string_without_spaces = array_to_string(string_to_array(word)).downcase
+  dictionary.each do |entry|
+    modified_string = string_without_spaces
 
-    dictionary.each do |entry|
-        hash[entry] = 0
-        string_without_spaces.include?(entry) ? hash[entry] += 1 : hash[entry] += 0
+    while modified_string.include?(entry)
+      if hash[entry].nil?
+        hash[entry] = string_without_spaces.include?(entry) ? 1 : 0
+      else
+        hash[entry] += string_without_spaces.include?(entry) ? 1 : 0
+      end
+      modified_string = modified_string.sub(entry, '')
     end
-    hash.select{|k, v| v > 0}
+  end
+  hash.select { |_k, v| v.positive? }
 end
 
 def string_to_array(string)
-    string.split(" ")
+  string.split(' ')
 end
 
 def array_to_string(array)
-    unformatted_string = array.join("").delete(" ")
-    unformatted_string
+  array.join('').delete(' ')
 end
 
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
